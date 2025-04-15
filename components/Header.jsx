be2ec7869435199d17e7e96eb44964bad9ee8d5d
Yasin <yasinpeker43@gmail.com>
2025-04-15 11:51:34 +0300

    Staged the logo and title.

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,6 +1,9 @@
 export default function Header() {
     return (
         <div id="header">
+         <img src="public/investment-calculator-logo.png" alt=""/>
+         <h1>React Investment Calculator</h1>
+
         </div>
     )
 }
