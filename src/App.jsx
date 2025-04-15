9314b594e3e3d0606f3fdb1802f7546cd8ac738a
Yasin <yasinpeker43@gmail.com>
2025-04-15 16:21:32 +0300

    Logo and title are done.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import Header from "../components/Header"
+import Header from "./components/Header"
+import { calculateInvestmentResults ,formatter } from "./util/investment"
 
 function App() {
   return (
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
+import logo from '../assets/investment-calculator-logo.png';
+
 export default function Header() {
     return (
         <div id="header">
-         <img src="public/investment-calculator-logo.png" alt=""/>
+         <img src={logo} alt="a logo showing a money bag"/>
          <h1>React Investment Calculator</h1>
-
         </div>
     )
 }
