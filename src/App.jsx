f16e174b8060a4ba378fc81d3e5e0c481bc69565
Yasin <yasinpeker43@gmail.com>
2025-04-16 17:18:57 +0300

    Calculation table is done.

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function handleChange(inputIdentifier,newValue) {
   setUserInput(prevUserInput => {
      return {
       ...prevUserInput,
-      [inputIdentifier]:newValue,
+      [inputIdentifier]: +newValue,
      }
   })
 }
--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,9 +1,41 @@
+import {calculateInvestmentResults,formatter } from '../util/investment.js';
+
 export default function Results({userInput}) {
 
-    console.log(userInput)
-      return (
-        <table id="resullt">
-            
+  const resultData = calculateInvestmentResults(userInput)
+  
+  const initialInvestment= resultData[0].valueEndOfYear - resultData[0].annualInvestment - resultData[0].interest
+  
+  return (
+        <table id="result">
+            <thead>
+          <tr>
+            <th>Year</th>
+            <th>Investment Value</th>
+            <th>Interest (Year)</th>
+            <th>Total Interest</th>
+            <th>Invested Capital</th>
+          </tr>
+         </thead>
+         <tbody>
+            {resultData.map((yearData)=> {
+            const totalInterest= yearData.valueEndOfYear - (yearData.annualInvestment * yearData.year )- initialInvestment;
+            const totalInvested=yearData.valueEndOfYear - totalInterest;
+                return(
+                     <tr key={yearData.year}>
+                        <td>{yearData.year}</td>
+                        <td>{formatter.format(yearData.valueEndOfYear)}</td>
+                        <td>{formatter.format(yearData.interest)}</td>
+                        <td>{formatter.format(totalInterest)}</td>
+                        <td>{formatter.format(totalInvested)}</td>
+                        
+                        
+                        
+                     </tr>
+                ) 
+                 
+            })}
+         </tbody>
         </table>
       )
 
